f075736963ee1608d1a7960c01e445b415708f5c
S73ph4n <stephanpissot@gmail.com>
2021-10-07 11:41:39 +0200

    Added JopliZot

---
+++ tools/joplizot/README.md
@@ -0,0 +1,16 @@
+# JopliZot.py (Work in Progress)
+Sync your Zotero library with Joplin.
+
+## Usage
+Install requirements:
+```bash
+pip install -r requirements.txt
+```
+
+Run the script:
+```bash
+python joplizot.py
+```
+
+## Configuration
+All the variables which this script asks for (JOPLIN\_TOKEN, ZOTERO\_LIBRARY\_ID, etc.) can be set from environment variables.
---
+++ tools/joplizot/joplizot.py
@@ -0,0 +1,51 @@
+import os, click, python_joplin
+from pyzotero import zotero
+
+confirm=True #ask before creating each note/ressource
+
+# Environment variables we need:
+ENV = {'JOPLIN_TOKEN':'', 'ZOTERO_LIBRARY_ID':'', 'ZOTERO_API_KEY':''} 
+
+def format_str(raw):
+    forbidden_chars = ['\n', '\r', '#', '\\', '\'', '\"']
+    return(''.join([c for c in raw if not c in forbidden_chars]))
+    #return(''.join([c for c in raw if c.isalnum() or c.isspace()]))
+    #return(''.join([c for c in raw if c.isprintable()]))
+
+# In case the environment variables are not set, let's set them :
+for VAR_NAME in ENV.keys():
+    ENV[VAR_NAME] = os.getenv(VAR_NAME)
+    if not ENV[VAR_NAME] : ENV[VAR_NAME] = click.prompt('Enter your '+VAR_NAME, type=str)
+    else : click.echo(VAR_NAME+ ' found in the environment.')
+
+#Prepare Joplin:
+click.echo('Connecting to Joplin...')
+jop = python_joplin.Joplin(ENV['JOPLIN_TOKEN']) #Connect to the Joplin API
+zot_notebook = jop.get_notebook_by_title('Zotero_Items', create_if_needed=True) #get the Joplin notebook we need
+click.echo('Joplin connection OK')
+
+#Prepare PyZotero:
+click.echo('Connecting to IMAP server...')
+zot = zotero.Zotero(ENV['ZOTERO_LIBRARY_ID'], 'user', ENV['ZOTERO_API_KEY'])
+click.echo('Zotero connection OK.\nFetching items...')
+items = zot.top(limit=50)
+click.echo('Items fetched.')
+
+#Process the items:
+for item in items:
+    if not confirm or click.confirm('Add item '+item['data']['title']+' ?', default=False):
+        title = format_str(item['data']['title']) #the title for our note
+        #title = msg.date_str + ' : ' + format_str(msg.subject) + ' [' + msg.from_  + ']' #the title for our note
+        note = inbox_notebook.get_note_by_title(title, create_if_needed=True) # Create note in notebook (or find it if it exists)
+        if note.body != '': continue #if there's already something, let's not change it
+        for prop_name in item['data'].keys():
+            python_joplin.tools.set_yaml(note, prop_name, item['data'][prop_name])
+
+        #TODO : add attachments
+        #for att in msg.attachments: 
+        #    if not confirm or click.confirm('\tAdd attachment '+att.filename+' ?', default=True):
+        #        att_jop = jop.new_ressource(att.filename, att.payload)
+        #        note.body += '['+att_jop.title+'](:/'+att_jop.id+')'
+        note.source = 'Zotero via JopliZot'
+        note.push() #Push updates to Joplin
+
